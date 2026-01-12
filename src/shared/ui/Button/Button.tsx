cf18d32552c5df5aec91356ac1a79361affeea31
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-12 10:44:32 +0900

    refactor: Button type prop 오버라이드 방지

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,11 +2,12 @@ import { buttonVariants } from '@shared/ui/Button/Button.variants';
 import type { ComponentPropsWithoutRef } from 'react';
 import type { VariantProps } from 'tailwind-variants';
 
-export type ButtonProps = ComponentPropsWithoutRef<'button'> & VariantProps<typeof buttonVariants>;
+export type ButtonProps = Omit<ComponentPropsWithoutRef<'button'>, 'type'> &
+  VariantProps<typeof buttonVariants>;
 
 export const Button = ({ children, variant, className, ...props }: ButtonProps) => {
   return (
-    <button className={buttonVariants({ variant, className })} {...props}>
+    <button type="button" className={buttonVariants({ variant, className })} {...props}>
       {children}
     </button>
   );
