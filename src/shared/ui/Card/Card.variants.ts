4f8e8edf3e6f79f618c298c85d61ea7ae46d994b
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-13 12:39:53 +0900

    feat: Card 컴포넌트 구현 및 디자인

---
+++ src/shared/ui/Card/Card.variants.ts
@@ -0,0 +1,51 @@
+import { tv } from 'tailwind-variants';
+
+export const cardVariants = tv({
+  base: [
+    'flex flex-col items-center',
+    'w-[204px] h-[299px]',
+    'gap-[14px]',
+    'bg-white',
+    'cursor-pointer',
+  ],
+
+  slots: {
+    imageWrapper: [
+      'relative',
+      'group',
+      'flex',
+      'p-[var(--padding-s)]',
+      'items-start',
+      'gap-[10px]',
+      'flex-1',
+      'self-stretch',
+      'rounded-[var(--radius-l)]',
+      'overflow-hidden',
+    ],
+
+    image: ['w-full h-full object-cover', 'rounded-[var(--radius-l)]'],
+
+    overlay: [
+      'absolute inset-0',
+      'rounded-[var(--radius-l)]',
+      'bg-white/20',
+      'opacity-0',
+      'transition-opacity',
+      'pointer-events-none',
+      'group-hover:opacity-100',
+    ],
+
+    title: [
+      'h-[48px]',
+      'self-stretch',
+      'overflow-hidden',
+      'line-clamp-2',
+      'typo-body-1',
+      'text-black',
+    ],
+
+    price: ['self-stretch', 'typo-body-2', 'font-semibold', 'text-black'],
+
+    date: ['self-stretch', 'typo-caption-2', 'font-normal', 'text-black'],
+  },
+});
