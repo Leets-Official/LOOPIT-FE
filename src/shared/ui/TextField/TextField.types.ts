326fa129a39e4286951c97a4a9a8cfd5bd75e3f5
YeBeenChoi <yebin607@naver.com>
2026-01-13 21:51:15 +0900

    feat: TextField props 타입 정의

---
+++ src/shared/ui/TextField/TextField.types.ts
@@ -0,0 +1,39 @@
+import type { textFieldVariants } from '@shared/ui/TextField/TextField.variants';
+import type { ComponentPropsWithoutRef } from 'react';
+import type { VariantProps } from 'tailwind-variants';
+
+/** TextField 타입 */
+export type TextFieldType = 'text' | 'char' | 'textarea' | 'price';
+
+/** Controlled / Uncontrolled */
+type ControlledTextFieldProps = {
+  value: string;
+  defaultValue?: never;
+};
+
+type UncontrolledTextFieldProps = {
+  value?: never;
+  defaultValue?: string;
+};
+
+/** 공통 Props */
+type BaseProps = {
+  type?: TextFieldType;
+  label?: string;
+  error?: boolean;
+  helperText?: string;
+  maxLength?: number;
+  showCharacterCount?: boolean;
+  className?: string;
+};
+
+/** input / textarea 공통 이벤트 */
+type CommonChangeEvent = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+
+export type TextFieldProps = BaseProps &
+  VariantProps<typeof textFieldVariants> &
+  (ControlledTextFieldProps | UncontrolledTextFieldProps) &
+  Omit<ComponentPropsWithoutRef<'input'>, 'type' | 'value' | 'defaultValue' | 'onChange'> &
+  Omit<ComponentPropsWithoutRef<'textarea'>, 'value' | 'defaultValue' | 'onChange'> & {
+    onChange?: (e: CommonChangeEvent) => void;
+  };
