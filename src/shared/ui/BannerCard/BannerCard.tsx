aab5da369f7e7bbcf314d3db93b7bddded80433d
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-16 16:29:22 +0900

    refactor:테스트 통과 위해 수정

--- src/shared/ui/BannerCard/BannerCard.tsx
+++ src/shared/ui/BannerCard/BannerCard.tsx
@@ -15,8 +15,14 @@ export type BannerCardProps = Omit<ComponentPropsWithoutRef<'div'>, 'title'> &
   };
 
 export const BannerCard = ({
-  title: titleContent,
-  description: descriptionText,
+  title: titleContent = (
+    <>
+      중고 전자기기
+      <br />
+      구매하기
+    </>
+  ),
+  description: descriptionText = '설명 최대 길이 2줄',
   buttonText = '바로가기',
   onClick,
   className,
@@ -27,6 +33,7 @@ export const BannerCard = ({
       data-testid="banner-card"
       className={base({ className })}
       role="button"
+      aria-label="banner-card"
       tabIndex={0}
       onClick={onClick}
       onKeyDown={(e) => {
