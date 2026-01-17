a03765b18fedf0512d90144600c30cb9ebc40a57
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-17 15:12:56 +0900

    refactor:overlay 제거

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -20,11 +20,11 @@ export const Modal = ({
   onCancel,
   onConfirm,
 }: ModalProps) => {
-  const { overlay, container, textGroup, buttonGroup, buttonWrapper } = modalVariants();
+  const { container, content, textGroup, buttonGroup, buttonWrapper } = modalVariants();
 
   return (
-    <div className={overlay()}>
-      <div className={container()}>
+    <div className={container()}>
+      <div className={content()}>
         <div className={textGroup()}>
           <p className="typo-body-2 text-black">{title}</p>
           {subtitle && <p className="typo-caption-2 truncate text-black">{subtitle}</p>}
@@ -32,23 +32,13 @@ export const Modal = ({
 
         <div className={buttonGroup()}>
           <div className={buttonWrapper()}>
-            <Button
-              variant="outline"
-              size="auto"
-              className="w-full"
-              onClick={onCancel}
-            >
+            <Button variant="outline" size="auto" className="w-full" onClick={onCancel}>
               {cancelText}
             </Button>
           </div>
 
           <div className={buttonWrapper()}>
-            <Button
-              variant="fill"
-              size="auto"
-              className="w-full"
-              onClick={onConfirm}
-            >
+            <Button variant="fill" size="auto" className="w-full" onClick={onConfirm}>
               {confirmText}
             </Button>
           </div>
