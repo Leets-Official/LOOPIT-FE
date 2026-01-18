c9e093db419ae36427d90273ea357cfe0d5c87da
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-18 17:51:00 +0900

    chore: build 폴더 git 추적 제거

--- build/client/assets/_index-BozhRgus.js
+++
@@ -1,6 +0,0 @@
-import { w as n } from './chunk-EPOLDU6W-XOETFyi3.js';
-import { j as o } from './index-CtGBG9o5.js';
-const i = n(function () {
-  return o.jsx('div', { children: o.jsx('h1', { children: 'Home' }) });
-});
-export { i as default };
