97b2e7cb227de1b7569507435736857a636d527b
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-18 21:57:21 +0900

    fix: 린트오류 수정

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -3,4 +3,4 @@ import CheckIcon from './common/check.svg?react';
 import DoneIcon from './common/done.svg?react';
 import SearchMagnifyingGlassIcon from './common/search-magnifying-glass.svg?react';
 
-export { CheckIcon, DoneIcon, SearchMagnifyingGlassIcon };
+export { CaretDownMdIcon, CheckIcon, DoneIcon, SearchMagnifyingGlassIcon };
