7fbbe5a37d0bc48bbf64090e9187ad433fc3ad5a
YeBeenChoi <yebin607@naver.com>
2026-01-19 19:11:34 +0900

    feat: FavoriteButton 스타일 variants 추가

---
+++ src/shared/ui/FavoriteButton/FavoriteButton.variants.ts
@@ -0,0 +1,8 @@
+import { tv } from 'tailwind-variants';
+
+export const favoriteButtonVariants = tv({
+  slots: {
+    root: ['group inline-flex items-center justify-center', 'w-[44px] h-[44px]', 'transition-colors'],
+    icon: ['w-[33px] h-[29.26px]'],
+  },
+});
