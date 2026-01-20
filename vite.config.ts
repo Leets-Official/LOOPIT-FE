959a174bd469272c0fb62af7747296312123a754
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-20 23:45:53 +0900

    chore(config): @ 별칭 추가

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   plugins: [reactRouter(), tailwindcss(), svgr()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       '@app': path.resolve(__dirname, './src/app'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@shared': path.resolve(__dirname, './src/shared'),
