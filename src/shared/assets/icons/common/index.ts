d7339ee7a01449f3b48c297d40e03aaee096b057
YeBeenChoi <yebin607@naver.com>
2026-01-20 21:30:32 +0900

    chore: 하트 아이콘 export 정리

--- src/shared/assets/icons/common/index.ts
+++ src/shared/assets/icons/common/index.ts
@@ -3,4 +3,3 @@ export { default as CheckIcon } from './check.svg?react';
 export { default as DoneIcon } from './done.svg?react';
 export { default as SearchMagnifyingGlassIcon } from './search-magnifying-glass.svg?react';
 export { default as HeartIcon } from './heart-outline.svg?react';
-export { default as HeartFilledIcon } from './heart-filled.svg?react';
