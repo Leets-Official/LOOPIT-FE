3783d8a47c0d04a2287d797d0a3712256f37b4a0
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-21 16:32:40 +0900

    fix(button) submit type 허용

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,16 +2,29 @@ import { buttonVariants } from '@shared/ui/Button/Button.variants';
 import type { ComponentPropsWithoutRef } from 'react';
 import type { VariantProps } from 'tailwind-variants';
 
-export type ButtonProps = Omit<ComponentPropsWithoutRef<'button'>, 'type'> & VariantProps<typeof buttonVariants>;
+export type ButtonProps = ComponentPropsWithoutRef<'button'> & VariantProps<typeof buttonVariants>;
 
-export const Button = ({ children, variant, size, className, onMouseUp, ...props }: ButtonProps) => {
+export const Button = ({
+  children,
+  variant,
+  size,
+  className,
+  onMouseUp,
+  type = 'button',
+  ...props
+}: ButtonProps) => {
   const handleMouseUp = (e: React.MouseEvent<HTMLButtonElement>) => {
     onMouseUp?.(e);
     e.currentTarget.blur();
   };
 
   return (
-    <button type="button" className={buttonVariants({ variant, size, className })} onMouseUp={handleMouseUp} {...props}>
+    <button
+      type={type}
+      className={buttonVariants({ variant, size, className })}
+      onMouseUp={handleMouseUp}
+      {...props}
+    >
       {children}
     </button>
   );
