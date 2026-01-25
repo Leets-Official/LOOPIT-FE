262bdf6e288334400490ea958fe91f0d88c4c829
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-26 00:31:39 +0900

    feat: 메인페이지에 Carousel3D 적용 및 배너카드 분리

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,3 +1,2 @@
-export { useOutsideClick } from './useOutsideClick';
-export { useEscapeKey } from './useEscapeKey';
-export { useToast } from './useToast';
+export { useClickOutside } from './useClickOutside';
+export { useImageUpload } from './useImageUpload';
