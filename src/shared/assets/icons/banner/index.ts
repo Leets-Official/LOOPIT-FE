93200eccda46c6f91e0df30941ac5ae18a2be347
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-26 03:04:16 +0900

    refactor: 대용량 SVG를 WebP로 교체하여 빌드 사이즈 최적화

--- src/shared/assets/icons/banner/index.ts
+++ src/shared/assets/icons/banner/index.ts
@@ -1,3 +1,3 @@
-export { default as GearIcon } from './geerIcon.svg';
-export { default as ShoppingIcon } from './shoppingIcon.svg';
-export { default as SellIcon } from './sellIcon.svg';
+export { default as GearIcon } from './geerIcon.webp';
+export { default as ShoppingIcon } from './shoppingIcon.webp';
+export { default as SellIcon } from './sellIcon.webp';
