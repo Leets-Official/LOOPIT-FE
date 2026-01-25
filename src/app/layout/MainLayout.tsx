5c7b3cc80567a4cc753e09579e886c6f03d811e9
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-25 22:39:58 +0900

    feat: 메인 레이아웃 헤더와 토스트 통합

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router';
 
 export default function MainLayout() {
   return (
-    <>
+    <div className="min-h-screen bg-white">
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
