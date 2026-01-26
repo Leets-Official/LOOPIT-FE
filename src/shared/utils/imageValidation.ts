fe1df9582287cc2b0a68f91cbc03186b32e1cabe
Wonjun Jung <jwj0620@gachon.ac.kr>
2026-01-26 15:43:39 +0900

    refactor:이미지 검증 share로 이동 및 파일명 변경

--- src/shared/utils/imageValidation.ts
+++ src/shared/utils/imageValidation.ts
@@ -1,12 +1,12 @@
-const IMAGE_TYPE_MESSAGE = '이미지 파일만 업로드해 주세요.';
-const IMAGE_SIZE_MESSAGE = '이미지는 5MB 이하로 업로드해 주세요.';
-
 type ImageValidationResult =
   | { ok: true }
   | {
       ok: false;
       message: string;
     };
+
+const IMAGE_TYPE_MESSAGE = '이미지 파일만 업로드해 주세요.';
+const IMAGE_SIZE_MESSAGE = '이미지는 5MB 이하로 업로드해 주세요.';
 
 export const validateImageFile = (file: File, maxBytes: number): ImageValidationResult => {
   if (!file.type.startsWith('image/')) {
