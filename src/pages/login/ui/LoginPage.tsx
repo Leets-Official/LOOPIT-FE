b75cb48071f1dfbaa081170a854a801c267627c3
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-26 11:39:30 +0900

    refactor: login 배경 이미지 WebP로 변환 및 배럴 파일 추가

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,11 +1,11 @@
-import loginBg from '@shared/assets/background/login_background.png';
+import { loginBackgroundImg } from '@shared/assets/background';
 import { kakaoLoginImg } from '@shared/assets/icons';
 import { Logo } from '@shared/ui/Logo';
 import { loginStyles as s } from './LoginPage.styles';
 
 export default function LoginPage() {
   return (
-    <div className={s.wrapper} style={{ backgroundImage: `url(${loginBg})` }}>
+    <div className={s.wrapper} style={{ backgroundImage: `url(${loginBackgroundImg})` }}>
       <div className="flex h-full w-full flex-col items-center justify-center gap-[26px] px-6 py-12">
         <Logo className="h-[36px] w-[192px] cursor-pointer" />
 
