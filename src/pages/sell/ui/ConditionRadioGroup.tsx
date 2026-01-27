d7e0f06a50e68af7b24b70da6f4be3b91c386a25
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-01-27 15:15:18 +0900

    feat: ConditionRadioGroup 컴포넌트 추가

---
+++ src/pages/sell/ui/ConditionRadioGroup.tsx
@@ -0,0 +1,35 @@
+import { RadioButton } from '@shared/ui/RadioButton/RadioButton';
+
+type ConditionOption<T extends string> = {
+  readonly label: string;
+  readonly value: T;
+};
+
+type ConditionRadioGroupProps<T extends string> = {
+  name: string;
+  options: ReadonlyArray<ConditionOption<T>>;
+  selectedValue: T | undefined;
+  onChange: (value: T) => void;
+};
+
+export const ConditionRadioGroup = <T extends string>({
+  name,
+  options,
+  selectedValue,
+  onChange,
+}: ConditionRadioGroupProps<T>) => {
+  return (
+    <div className="flex items-center gap-[22px]">
+      {options.map((option) => (
+        <RadioButton
+          key={option.value}
+          name={name}
+          label={option.label}
+          checked={selectedValue === option.value}
+          onChange={() => onChange(option.value)}
+          className="[&_span:last-child]:whitespace-nowrap"
+        />
+      ))}
+    </div>
+  );
+};
