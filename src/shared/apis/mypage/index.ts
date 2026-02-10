6b5b7b591af0912aa00ac413c30123ae88a8f4c6
YeBeenChoi <yebin607@naver.com>
2026-02-10 20:58:08 +0900

    fix: 마이페이지 API 타입 export 누락 보완

--- src/shared/apis/mypage/index.ts
+++ src/shared/apis/mypage/index.ts
@@ -1,4 +1,5 @@
 export { getMyPageBuyHistory, getMyPageProfile, getMyPageSellHistory } from './api';
 export { mypageKeys } from './keys';
 export { useMyPageBuyHistoryQuery, useMyPageProfileQuery, useMyPageSellHistoryQuery } from './queries';
+export type { TradeHistoryQueryStatus } from './api';
 export type { MyPageProfile, TradeHistoryItem, TradeHistoryStatus } from './types';
